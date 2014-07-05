dfa8419d14c490f9acd04a4fb22c1c4c603a0c84
Carlos Galdino <carloshsgaldino@gmail.com>
2014-07-04 23:23:30 -0300

    Initial commit.

---
+++ script.rb
@@ -0,0 +1,31 @@
+require 'json'
+
+def item_xml(options = {})
+  <<-ITEM
+  <item arg="#{options[:arg]}" uid="#{options[:uid]}">
+    <title>#{options[:title]}</title>
+    <subtitle>#{options[:subtitle]}</subtitle>
+    <icon>#{options[:path]}</icon>
+  </item>
+  ITEM
+end
+
+def match?(word, query)
+  word.match(/#{query}/i)
+end
+
+gifs = JSON.parse(File.read(File.open(File.expand_path('~/.boom'))))['lists'].first['gifs']
+
+query = Regexp.escape(ARGV.first).delete(':')
+
+matches = gifs.select { |h| match?(h.keys.first, query) || match?(h.values.first, query) }
+
+items = matches.map do |elem|
+  key = elem.keys.first
+  value = elem.values.first
+  item_xml({ :arg => value, :uid => key, :title => key, :subtitle => value })
+end.join
+
+output = "<?xml version='1.0'?>\n<items>\n#{items}</items>"
+
+puts output
